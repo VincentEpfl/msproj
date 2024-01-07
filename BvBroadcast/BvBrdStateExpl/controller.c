4c56bb6654cdb2057a6e900dcdd9827c7ad8e1e0
Vincent Parodi <vincent@MBP-de-Vincent.lan>
2024-01-07 19:38:15 +0100

    bv brd recv once

--- BvBroadcast/BvBrdStateExpl/controller.c
+++ BvBroadcast/BvBrdStateExpl/controller.c
@@ -584,6 +584,7 @@ bool canDeliverState(int posInForkPath, int stateToUpdate, int sendIndex, int re
 bool canDeliverRecvState(int stateToUpdate, int recvIndex)
 {
   bool recvDeliver = true;
+  /*
   if (msgbuffer[recvIndex].numDelivered > 0)
   {
     for (int f = 0; f < msgbuffer[recvIndex].numDelivered; f++)
@@ -598,6 +599,12 @@ bool canDeliverRecvState(int stateToUpdate, int recvIndex)
       }
     }
   }
+  */
+
+  if (msgbuffer[recvIndex].numDelivered > 0) {
+    recvDeliver = false;
+  }
+
   return recvDeliver;
 }
 
@@ -919,7 +926,7 @@ int handleMessagePair(int recvIndex, int sendIndex, int fd, bool recv)
     // add msg to history
     addMsgToHistory(forkid0, msgbuffer[sendIndex].from, msgbuffer[sendIndex].to, msgbuffer[sendIndex].msg);
 
-    if (msgbuffer[sendIndex].from == 2) // msgbuffer[sendIndex].from == 2  msgbuffer[sendIndex].from == 3
+    if (false) // msgbuffer[sendIndex].from == 2  msgbuffer[sendIndex].from == 3
     {
       // Try to send the message with the opposite value
       //printf("[Controller] send opposite msg to receiver\n");
