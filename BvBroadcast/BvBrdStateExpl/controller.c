d7648c261574caacc9da0f81500dad3453c4400f
Vincent Parodi <vincent@MacBook-Pro-de-Vincent.local>
2024-01-11 17:25:00 +0100

    bv brd different scenario

--- BvBroadcast/BvBrdStateExpl/controller.c
+++ BvBroadcast/BvBrdStateExpl/controller.c
@@ -987,7 +987,7 @@ int handleMessagePair(int recvIndex, int sendIndex, int fd, bool recv)
     // add msg to history
     addMsgToHistory(forkid0, msgbuffer[sendIndex].from, msgbuffer[sendIndex].to, msgbuffer[sendIndex].msg, 0);
 
-    if (false) // msgbuffer[sendIndex].from == 2  msgbuffer[sendIndex].from == 3
+    if (msgbuffer[sendIndex].from == 2) // msgbuffer[sendIndex].from == 2  msgbuffer[sendIndex].from == 3
     {
       // Try to send the message with the opposite value
       //printf("[Controller] send opposite msg to receiver\n");
