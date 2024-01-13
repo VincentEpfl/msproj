5a5a44d03f040ce47489bcd69436343868df4aba
Vincent Parodi <vincent@MBP-de-Vincent.lan>
2024-01-13 20:28:51 +0100

    bv brd different scenario

--- BvBroadcast/BvBrdStateExpl/controller.c
+++ BvBroadcast/BvBrdStateExpl/controller.c
@@ -1054,7 +1054,7 @@ int handleMessagePair(int recvIndex, int sendIndex, int fd, bool recv)
     deliver_message_forkid(recvIndex, forkid0);
     deliver_message_forkid(sendIndex, forkid0);
 
-    if (msgbuffer[sendIndex].from == 2) // msgbuffer[sendIndex].from == 2  msgbuffer[sendIndex].from == 3
+    if (msgbuffer[sendIndex].from == 2 || msgbuffer[sendIndex].from == 1) // msgbuffer[sendIndex].from == 2  msgbuffer[sendIndex].from == 3
     {
       // Try to send the message with the opposite value
       //printf("[Controller] send opposite msg to receiver\n");
