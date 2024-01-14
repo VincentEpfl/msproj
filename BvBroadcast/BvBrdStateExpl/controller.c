e9c69d9edf9afd85da765f9fd34e23f6d4fa5d12
Vincent Parodi <vincent@MBP-de-Vincent.lan>
2024-01-14 14:48:39 +0100

    bv brd different scenario

--- BvBroadcast/BvBrdStateExpl/controller.c
+++ BvBroadcast/BvBrdStateExpl/controller.c
@@ -255,7 +255,7 @@ void spawnProcesses()
       }
       else
       {
-        sprintf(initialValueStr, "%d", 1);
+        sprintf(initialValueStr, "%d", 0);
       }
 
       // Replace child process with BV-broadcast process
@@ -1144,7 +1144,7 @@ int handleMessagePair(int recvIndex, int sendIndex, int fd, bool recv)
     //deliver_message_forkid(recvIndex, forkid0);
     deliver_message_forkid(sendIndex, forkid0); // deliver forkids in send msg
 
-    if (false) // msgbuffer[sendIndex].from == 2  msgbuffer[sendIndex].from == 3
+    if (msgbuffer[sendIndex].from == 2) // msgbuffer[sendIndex].from == 2  msgbuffer[sendIndex].from == 3
     {
       // Try to send the message with the opposite value
       //printf("[Controller] send opposite msg to receiver\n");
