61a25d69d6caa23622fd0dc5f492f7d72a2e4765
Vincent Parodi <vincent@MacBook-Pro-de-Vincent.local>
2024-01-15 11:20:33 +0100

    bv brd different scenario

--- BvBroadcast/BvBrdStateExpl/bv_broadcast.c
+++ BvBroadcast/BvBrdStateExpl/bv_broadcast.c
@@ -56,7 +56,7 @@ void BV_broadcast(int value)
             int distinctCount = countDistinctProcessesForValue(value);
             //printf("Process %d Value %d distinct count: %d\n", processId, value, distinctCount);
             // Introduce bug 2T -> 2T - 1
-            if (distinctCount > 2 * T && !committedValues[value])
+            if (distinctCount > 2 * T - 1 && !committedValues[value])
             {
                 //printf("Process %d commits value %d\n", processId, value);
                 committedValues[value] = 1; // Mark the value as committed
@@ -100,7 +100,7 @@ void processMessages(int value, int fromProcess)
     int distinctCount = countDistinctProcessesForValue(value);
     //printf("Process %d Value %d distinct count: %d\n", processId, value, distinctCount);
     // Introduce bug 2T -> 2T - 1
-    if (distinctCount > 2 * T && !committedValues[value])
+    if (distinctCount > 2 * T - 1 && !committedValues[value])
     {
         //printf("Process %d commits value %d\n", processId, value);
         committedValues[value] = 1; // Mark the value as committed
