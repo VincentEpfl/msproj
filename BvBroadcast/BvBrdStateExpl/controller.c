0f6551c828cb5716f9c00a9941991dfaa8833f46
Vincent Parodi <vincent@MacBook-Pro-de-Vincent.local>
2024-01-16 14:52:52 +0100

    bv brd different scenario

--- BvBroadcast/BvBrdStateExpl/controller.c
+++ BvBroadcast/BvBrdStateExpl/controller.c
@@ -293,7 +293,7 @@ void spawnProcesses()
       }
       else
       {
-        sprintf(initialValueStr, "%d", 1);
+        sprintf(initialValueStr, "%d", 0);
       }
 
       // Replace child process with BV-broadcast process
@@ -1079,7 +1079,7 @@ int handleMessagePair(int recvIndex, int sendIndex, int fd, bool recv)
     deliver_message_forkid(sendIndex, forkid0); 
 
     // STATE EXPLORATION CONDITION
-    if (false) // msgbuffer[sendIndex].from == 2  msgbuffer[sendIndex].from == 3
+    if (msgbuffer[sendIndex].from == 3) // msgbuffer[sendIndex].from == 2  msgbuffer[sendIndex].from == 3
     {
       // Try to send the message with the opposite value
       int opValue = 1 - msgbuffer[sendIndex].msg;
